6177b8422870716e23cd9db0822a1276c2f56e20
asgerb <a@asgerbehnckejacobsen.dk>
2018-08-22 12:12:14 +0200

    CreateView

--- lib/mongoid_occurrence_views/create_view.rb
+++ lib/mongoid_occurrence_views/create_view.rb
@@ -1,15 +1,25 @@
 module MongoidOccurrenceViews
-  class CreateView < Struct.new(:view_name, :view_on, :pipeline)
+  class CreateView
+    def initialize(name:, collection:, pipeline:)
+      @name = name
+      @collection = collection
+      @pipeline = pipeline
+    end
+
     def self.call(*args)
       new(*args).call
     end
 
     def call
-      Mongoid.clients.each do |name, _|
-        client = Mongoid.client(name)
-        next if client.collections.map(&:name).include?(view_name)
-        client.command(create: view_name, viewOn: view_on, pipeline: pipeline)
+      Mongoid.clients.each do |client_name, _|
+        client = Mongoid.client(client_name)
+        next if client.collections.map(&:name).include?(name)
+        client.command(create: name, viewOn: collection, pipeline: pipeline)
       end
     end
+
+    private
+
+    attr_reader :name, :collection, :pipeline
   end
 end
