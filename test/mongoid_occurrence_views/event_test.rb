7761590d81a94c161458e32016a37c7d3b8afb8c
asgerb <a@asgerbehnckejacobsen.dk>
2018-08-24 17:56:51 +0200

    Cleanup event test

--- test/mongoid_occurrence_views/event_test.rb
+++ test/mongoid_occurrence_views/event_test.rb
@@ -1,16 +1,7 @@
 require "test_helper"
 
 describe MongoidOccurrenceViews::Event do
-  let(:event) { ::Event.new }
+  let(:event) { build(:event, :today) }
 
   it { event.must_respond_to :occurrences }
-  it { event.class.occurrences_view_name.must_equal 'events__occurrences_view' }
-  it { event.class.expanded_occurrences_view_name.must_equal 'events__expanded_occurrences_view' }
-
-  describe 'views' do
-    let(:collection_names) { Mongoid.clients.map{ |name, _| Mongoid.client(name).collections.map(&:name) }.flatten }
-
-    it { collection_names.must_include event.class.occurrences_view_name }
-    it { collection_names.must_include event.class.expanded_occurrences_view_name }
-  end
 end
