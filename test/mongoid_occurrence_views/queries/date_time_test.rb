b81c7f737ec82a5885823d0439368cc19b717354
asgerb <a@asgerbehnckejacobsen.dk>
2018-08-24 16:22:03 +0200

    Refactor DateTime test to use factories

--- test/mongoid_occurrence_views/queries/date_time_test.rb
+++ test/mongoid_occurrence_views/queries/date_time_test.rb
@@ -1,42 +1,41 @@
 require 'test_helper'
 
 describe 'Querying Events' do
-  let(:start_date) { DateTime.now.beginning_of_day + 4.hours }
-  let(:end_date) { start_date + 2.hours }
-  let(:all_day) { false }
-  let(:schedule) { nil }
-  let(:occurrence) { Occurrence.new(dtstart: start_date, dtend: end_date, all_day: all_day, schedule: schedule) }
-  let(:event) { Event.new(occurrences: [occurrence]) }
-
-  let(:query_date) { start_date }
-  let(:query) { MongoidOccurrenceViews::Queries::DateTime.criteria(Event, query_date) }
+  let(:query) { MongoidOccurrenceViews::Queries::DateTime.criteria(Event, query_date_time) }
+  let(:wrong_query) { MongoidOccurrenceViews::Queries::DateTime.criteria(Event, query_date_time - 1.year) }
 
   before { event.save! }
 
   describe 'spanning one day' do
+    let(:event) { build(:event, :today) }
+    let(:query_date_time) { DateTime.now.beginning_of_day + 5.hours }
+
     it { query.count.must_equal 1 }
+    it { wrong_query.count.must_equal 0 }
     it { within_expanded_occurrences { query.count.must_equal 1 } }
-    # TODO: TBD when we know if the normal occurrences view is needed
+    it { within_expanded_occurrences { wrong_query.count.must_equal 0 } }
     # it { within_occurrences { query.count.must_equal 1 } }
   end
 
   describe 'spanning multiple days' do
-    let(:end_date) { start_date + 2.days }
-    let(:query_date) { start_date + 1.day }
+    let(:event) { build(:event, :today_until_tomorrow) }
+    let(:query_date_time) { event.occurrences.first.dtstart + 1.day }
 
     it { query.count.must_equal 1 }
+    it { wrong_query.count.must_equal 0 }
     it { within_expanded_occurrences { query.count.must_equal 1 } }
-    # TODO: TBD when we know if the normal occurrences view is needed
+    it { within_expanded_occurrences { wrong_query.count.must_equal 0 } }
     # it { within_occurrences { query.count.must_equal 1 } }
   end
 
   describe 'recurring' do
-    let(:schedule) { IceCube::Schedule.new(start_date.to_time) { |s| s.add_recurrence_rule IceCube::Rule.daily(1).count(7) } }
-    let(:query_date) { start_date + 4.days }
+    let(:event) { build(:event, :recurring_daily_this_week) }
+    let(:query_date_time) { DateTime.now.beginning_of_day + 2.days }
 
     it { query.count.must_equal 1 }
+    it { wrong_query.count.must_equal 0 }
     it { within_expanded_occurrences { query.count.must_equal 1 } }
-    # TODO: TBD when we know if the normal occurrences view is needed
+    it { within_expanded_occurrences { wrong_query.count.must_equal 0 } }
     # it { within_occurrences { query.count.must_equal 1 } }
   end
 
@@ -52,43 +51,41 @@ describe 'Querying Events' do
 end
 
 describe 'Querying Parent with Embedded Events' do
-  let(:start_date) { DateTime.now.beginning_of_day + 4.hours }
-  let(:end_date) { start_date + 2.hours }
-  let(:all_day) { false }
-  let(:schedule) { nil }
-  let(:occurrence) { Occurrence.new(dtstart: start_date, dtend: end_date, all_day: all_day, schedule: schedule) }
-  let(:embedded_event) { EmbeddedEvent.new(occurrences: [occurrence]) }
-  let(:event_parent) { EventParent.new(embedded_events: [embedded_event]) }
-
-  let(:query_date) { start_date }
-  let(:query) { MongoidOccurrenceViews::Queries::DateTime.criteria(EventParent, query_date) }
+  let(:query) { MongoidOccurrenceViews::Queries::DateTime.criteria(EventParent, query_date_time) }
+  let(:wrong_query) { MongoidOccurrenceViews::Queries::DateTime.criteria(Event, query_date_time - 1.year) }
 
   before { event_parent.save! }
 
   describe 'spanning one day' do
+    let(:event_parent) { build(:event_parent, :today) }
+    let(:query_date_time) { DateTime.now.beginning_of_day + 5.hours }
+
     it { query.count.must_equal 0 }
+    it { wrong_query.count.must_equal 0 }
     it { within_expanded_occurrences { query.count.must_equal 1 } }
-    # TODO: TBD when we know if the normal occurrences view is needed
+    it { within_expanded_occurrences { wrong_query.count.must_equal 0 } }
     # it { within_occurrences { query.count.must_equal 1 } }
   end
 
   describe 'spanning multiple days' do
-    let(:end_date) { start_date + 2.days }
-    let(:query_date) { start_date + 1.day }
+    let(:event_parent) { build(:event_parent, :today_until_tomorrow) }
+    let(:query_date_time) { DateTime.now.beginning_of_day + 1.day }
 
     it { query.count.must_equal 0 }
+    it { wrong_query.count.must_equal 0 }
     it { within_expanded_occurrences { query.count.must_equal 1 } }
-    # TODO: TBD when we know if the normal occurrences view is needed
+    it { within_expanded_occurrences { wrong_query.count.must_equal 0 } }
     # it { within_occurrences { query.count.must_equal 1 } }
   end
 
   describe 'recurring' do
-    let(:schedule) { IceCube::Schedule.new(start_date.to_time) { |s| s.add_recurrence_rule IceCube::Rule.daily(1).count(7) } }
-    let(:query_date) { start_date + 4.days }
+    let(:event_parent) { build(:event_parent, :recurring_daily_this_week) }
+    let(:query_date_time) { DateTime.now.beginning_of_day + 3.days }
 
     it { query.count.must_equal 0 }
+    it { wrong_query.count.must_equal 0 }
     it { within_expanded_occurrences { query.count.must_equal 1 } }
-    # TODO: TBD when we know if the normal occurrences view is needed
+    it { within_expanded_occurrences { wrong_query.count.must_equal 0 } }
     # it { within_occurrences { query.count.must_equal 1 } }
   end
 
