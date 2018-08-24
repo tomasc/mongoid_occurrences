de7bdc8777cc6fd4cf37131ce47c3b24eaa1b594
asgerb <a@asgerbehnckejacobsen.dk>
2018-08-24 16:53:48 +0200

    Test of HasViewsOnOccurrences

--- test/mongoid_occurrence_views/event/has_views_on_occurences_test.rb
+++ test/mongoid_occurrence_views/event/has_views_on_occurences_test.rb
@@ -1,1 +1,15 @@
-# TODO: add this test
+require "test_helper"
+
+describe MongoidOccurrenceViews::Event::HasViewsOnOccurrences do
+  let(:event_parent) { ::EventParent.new }
+
+  it { event_parent.class.occurrences_view_name.must_equal 'event_parents__occurrences_view' }
+  it { event_parent.class.expanded_occurrences_view_name.must_equal 'event_parents__expanded_occurrences_view' }
+
+  describe 'views' do
+    let(:collection_names) { Mongoid.clients.map{ |name, _| Mongoid.client(name).collections.map(&:name) }.flatten }
+
+    it { collection_names.must_include event_parent.class.occurrences_view_name }
+    it { collection_names.must_include event_parent.class.expanded_occurrences_view_name }
+  end
+end
