ab4f9d5b96dd679bbbefb4641229bae3e3f23ac7
Tomáš Celizna <tomas.celizna@gmail.com>
2018-08-24 13:27:38 +0200

    cleanup

--- test/mongoid_occurrence_views/event_test.rb
+++ test/mongoid_occurrence_views/event_test.rb
@@ -3,17 +3,9 @@ require "test_helper"
 describe MongoidOccurrenceViews::Event do
   let(:event) { ::Event.new }
 
-  describe 'relations' do
-    it { event.must_respond_to :occurrences }
-  end
-
-  describe '.occurrences_view_name' do
-    it { event.class.occurrences_view_name.must_equal 'events__occurrences_view' }
-  end
-
-  describe '.expanded_occurrences_view_name' do
-    it { event.class.expanded_occurrences_view_name.must_equal 'events__expanded_occurrences_view' }
-  end
+  it { event.must_respond_to :occurrences }
+  it { event.class.occurrences_view_name.must_equal 'events__occurrences_view' }
+  it { event.class.expanded_occurrences_view_name.must_equal 'events__expanded_occurrences_view' }
 
   describe 'views' do
     let(:collection_names) { Mongoid.clients.map{ |name, _| Mongoid.client(name).collections.map(&:name) }.flatten }
