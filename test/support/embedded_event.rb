b61c5a5613185211c778500a290e9f68a12ca22a
asgerb <a@asgerbehnckejacobsen.dk>
2018-08-30 18:16:15 +0200

    Cleanup tests

--- test/support/embedded_event.rb
+++ test/support/embedded_event.rb
@@ -1,7 +1,7 @@
 class EmbeddedEvent
   include Mongoid::Document
-  include MongoidOccurrenceViews::Event
 
+  include MongoidOccurrenceViews::Event
   embeds_many_occurrences class_name: 'Occurrence'
   embedded_in :owner, class_name: 'EventParent'
 end
--- test/support/occurrence.rb
+++ test/support/occurrence.rb
@@ -1,6 +1,6 @@
 class Occurrence
   include Mongoid::Document
-  include MongoidOccurrenceViews::Event::Occurrence
 
+  include MongoidOccurrenceViews::Event::Occurrence
   embedded_in_event class_name: 'Event'
 end
