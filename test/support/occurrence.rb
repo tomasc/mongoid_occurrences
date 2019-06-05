e8fd5d1c170ebc5846d1f862df32020876b3c8ce
Tomáš Celizna <tomas.celizna@gmail.com>
2019-06-05 13:34:06 +0200

    ADD: default sort order on the `#occurrences` association (#10)

--- test/support/occurrence.rb
+++ test/support/occurrence.rb
@@ -2,5 +2,5 @@ class Occurrence
   include Mongoid::Document
   include MongoidOccurrences::Occurrence
 
-  embedded_in_event :event, class_name: '::Event'
+  embedded_in_event :event, class_name: 'Event'
 end
